cc180b9775bea96223b351249506f0903d1972b8
Troy Fortin <troy@firelock.ai>
2026-03-28 05:33:16 -0400

    chore: remove tracked target/ directory
    Signed-off-by: Troy Fortin <troy@firelock.ai>

--- target/debug/deps/kin_search-4527c97bac9daa63.d
+++
@@ -1,7 +0,0 @@
-/Users/troyfortinjr/GitHub/kin-ecosystem/kin-search/target/debug/deps/kin_search-4527c97bac9daa63.d: src/lib.rs
-
-/Users/troyfortinjr/GitHub/kin-ecosystem/kin-search/target/debug/deps/libkin_search-4527c97bac9daa63.rlib: src/lib.rs
-
-/Users/troyfortinjr/GitHub/kin-ecosystem/kin-search/target/debug/deps/libkin_search-4527c97bac9daa63.rmeta: src/lib.rs
-
-src/lib.rs:
